ef9a5f3c6fdfb4cff75b85d2adc5c32b13c5fe2d
Michael Lee <mlee@wilbergroup.com>
2017-03-13 11:29:20 -0500

    Create fresh params when new bucket provided

--- src/S3BucketStreamZip.php
+++ src/S3BucketStreamZip.php
@@ -27,6 +27,11 @@ class S3BucketStreamZip
     private $s3Client;
 
     /**
+     * @var object
+     */
+    private $params;
+
+    /**
      * Create a new ZipStream object.
      *
      * @param array $auth - AWS key and secret
@@ -41,8 +46,8 @@ class S3BucketStreamZip
         // S3 User in $this->auth should have permission to execute ListBucket on any buckets
         // AND GetObject on any object with which you need to interact.
         $this->s3Client = new S3Client([
-            'version'     => $this->auth['version'],
-            'region'      => $this->auth['region'],
+            'version'     => (isset($this->auth['version'])) ? $this->auth['version'] : 'latest',
+            'region'      => (isset($this->auth['refion'])) ? $this->auth['region'] : 'us-east',
             'credentials' => [
                 'key'    => $this->auth['key'],
                 'secret' => $this->auth['secret'],
@@ -52,6 +57,29 @@ class S3BucketStreamZip
         // Register the stream wrapper from an S3Client object
         // This allows you to access buckets and objects stored in Amazon S3 using the s3:// protocol
         $this->s3Client->registerStreamWrapper();
+    }
+
+    public function bucket($bucket)
+    {
+        $this->params = new S3Params($bucket);
+
+        return $this;
+    }
+
+    public function prefix($prefix)
+    {
+        $this->params->setParam('Prefix', $prefix);
+
+        return $this;
+    }
+
+    public function addParams(array $params)
+    {
+        foreach($params as $key => $value) {
+            $this->params->setParam($key, $value);
+        }
+
+        return $this;
     }
 
     /**
@@ -68,10 +96,11 @@ class S3BucketStreamZip
      *
      * ['Bucket' => 'YOUR_BUCKET']
      */
-    public function send($filename, $params)
+    public function send($filename)
     {
+        $params = $this->params->getParams();
+
         $this->validateParams($params);
-        $this->doesDirectoryExist($params);
 
         $zip = new ZipStream($filename);
         // The iterator fetches ALL of the objects without having to manually loop over responses.
@@ -95,24 +124,6 @@ class S3BucketStreamZip
 
         // Finalize the zip file.
         $zip->finish();
-    }
-
-    protected function doesDirectoryExist($params)
-    {
-        // Maybe this isn't an actual key, but a prefix.
-        // Do a prefix listing of objects to determine.
-        $command = $this->s3Client->getCommand('listObjects', $params);
-
-        try {
-            $result = $this->s3Client->execute($command);
-
-            return $result['Contents'] || $result['CommonPrefixes'];
-        } catch (S3Exception $e) {
-            if ($e->getStatusCode() === 403) {
-                return false;
-            }
-            throw $e;
-        }
     }
 
     private function validateAuth($auth)
@@ -140,6 +151,26 @@ class S3BucketStreamZip
     {
         if (!isset($params['Bucket'])) {
             throw new InvalidParameterException('$params parameter to send() requires a `Bucket` attribute (with a capital B)');
+        }
+
+        $this->doesDirectoryExist($params);
+    }
+
+    protected function doesDirectoryExist($params)
+    {
+        // Maybe this isn't an actual key, but a prefix.
+        // Do a prefix listing of objects to determine.
+        $command = $this->s3Client->getCommand('listObjects', $params);
+
+        try {
+            $result = $this->s3Client->execute($command);
+
+            return $result['Contents'] || $result['CommonPrefixes'];
+        } catch (S3Exception $e) {
+            if ($e->getStatusCode() === 403) {
+                return false;
+            }
+            throw $e;
         }
     }
 }
